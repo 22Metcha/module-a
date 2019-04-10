775d8691a089ffdedd3153eac2419a3b836013d5
22Metcha <49483183+22Metcha@users.noreply.github.com>
2019-04-10 12:44:46 -0400

    say hello
    give the world a nice hello

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+# Hailey metcalf
+# 04/10/2019
+
+################
+#  Hello World #
+#              #
+#  Our First   #
+#  Python      #
+#  Application #
+################
+
+#print "Hello, World" to the screen.
+print("Hello, World")
